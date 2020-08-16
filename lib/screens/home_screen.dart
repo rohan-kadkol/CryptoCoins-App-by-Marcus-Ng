1b7f35901b541b6f78d6ddcedea2a1df16633f0c
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-08-16 18:05:57 -0500

    Added first iteration of the HomeScreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Coins'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Theme.of(context).primaryColor,
+              Colors.grey[900],
+            ],
+          ),
+        ),
+        // child: ListView.builder(itemBuilder: null),
+      ),
+    );
+  }
+}
